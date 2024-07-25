1c4bcf39f5e4b3a792fd9109c3be118e211df52a
Nathan Covey <156732802+nathancovey@users.noreply.github.com>
2024-07-25 15:49:39 -0400

    Fixes

--- gmail_to_discord.py
+++ gmail_to_discord.py
@@ -6,7 +6,6 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
